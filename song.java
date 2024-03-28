f1a66403829ef184e555b658ce0fff773ed14bca
Erjona Haka <111374968+jonah05@users.noreply.github.com>
2024-03-28 17:50:18 +0100

    song.java

---
+++ song.java
@@ -0,0 +1,16 @@
+public class Song {
+    private String title;
+    private double duration;
+    
+    public Song(String title, double duration ){
+        this.title = title;
+        this.duration=duration;
+    }
+    public String getTitle(){
+        return title;
+    }
+    @Override
+    public String toString(){
+        return this.title + ": " + this.duration;
+    }
+}
